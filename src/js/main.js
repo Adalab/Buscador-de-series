fd3e1d7dca9e7a0138c3e22a0c8b6f7bb0ce4e11
Marina Delgado <marinadelges@gmail.com>
2021-02-08 21:11:06 +0100

    paint movies

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,46 @@
-/* eslint-disable no-console */
 'use strict';
 
-let series = [];
+let movies = [];
 const inputElement = document.querySelector('.js-input');
 const inputValue = inputElement.value;
-//CREAR función y meter api dentro
+const moviesContainer = document.querySelector('.js-fav-list');
+const placeholderImg =
+  'https://via.placeholder.com/150/0000FF/808080 ?Text=Digital.com';
+//función api
 function getDataFromApi() {
   fetch(`http://api.tvmaze.com/search/shows?q=${inputValue}`)
     .then((response) => response.json())
     .then((data) => {
       for (let index = 0; index < data.length; index++) {
         const showList = data[index].show;
-        series.push(showList);
+        movies.push(showList);
       }
-      console.log(data);
+      paintMovies();
     });
-} //start api
+}
+// Pintar las películas
+
+function paintMovies() {
+  let htmlCode = '';
+  for (let index = 0; index < movies.length; index++) {
+    const name = movies[index].name;
+    const image = movies[index].image;
+    const id = movies[index].id;
+    htmlCode += `<li class="movie-target js-movieTarget" id="${id}">`;
+    htmlCode += `<div class="imageContainer"`;
+    if (image) {
+      htmlCode += `<img src"${image.medium}" class="movie-img js-movie-image" alt="img"/>`;
+    } else {
+      htmlCode += `<img src"${placeholderImg}" class="movie-img js-movie-image" alt="img"/>`;
+      htmlCode += `</div>`;
+    }
+
+    htmlCode += `<h3 class="movie-title js-movieTitle">${name}</h3>`;
+    htmlCode += `</li>`;
+  }
+  moviesContainer.innerHTML = htmlCode;
+}
+
+paintMovies();
+//start api
 getDataFromApi();
