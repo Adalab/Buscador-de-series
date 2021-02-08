5f6680e1d5d57170b83b6fd222dd82ce63564338
Marina Delgado <marinadelges@gmail.com>
2021-02-08 19:50:23 +0100

    Start page and get data from api

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,19 @@
+/* eslint-disable no-console */
 'use strict';
 
-console.log('>> Ready :)');
+let series = [];
+const inputElement = document.querySelector('.js-input');
+const inputValue = inputElement.value;
+//CREAR función y meter api dentro
+function getDataFromApi() {
+  fetch(`http://api.tvmaze.com/search/shows?q=${inputValue}`)
+    .then((response) => response.json())
+    .then((data) => {
+      for (let index = 0; index < data.length; index++) {
+        const showList = data[index].show;
+        series.push(showList);
+      }
+      console.log(data);
+    });
+} //start api
+getDataFromApi();
